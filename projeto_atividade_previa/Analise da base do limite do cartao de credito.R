622b16051f362d70307907e7c7618af50cbb084a
vidal_root <vidal.desenv@gmail.com>
2021-05-16 22:03:38 -0300

    Analise detalhada na base

--- projeto_atividade_previa/Analise da base do limite do cartao de credito.R
+++ projeto_atividade_previa/Analise da base do limite do cartao de credito.R
@@ -6,63 +6,80 @@ cartao_credito <- data.frame(read_excel("D:/Estudos/MBA - Engenharia de Dados/CP
 #Verificando dados ausentes na base geral
 md.pattern(cartao_credito)
 
-head(cartao_credito)
-
 #separa dados de analise
 analise <- data.frame(
-   SEXO = cut(cartao_credito$SEX
-              , breaks = c(0,1,2)
-              , labels = c("M","F"))
+  SEXO = cut(cartao_credito$SEX
+             , breaks = c(0,1,2)
+             , labels = c("M","F"))
   ,EDUCACAO = cut(cartao_credito$EDUCATION
-                    , breaks = c(0,1,2,3,4)
-                    , labels = c("pos-graduacao","universidade","ensino medio","outros"))
+                  , breaks = c(0,1,2,3,4)
+                  , labels = c("pos-graduacao","universidade","ensino medio","outros"))
   ,ESTADO_CIVIL = cut(cartao_credito$MARRIAGE
                       , breaks = c(0,1,2,3)
                       , labels = c("casado","solteiro","outros"))
-  ,IDADE = c(cartao_credito$AGE)
+  #,IDADE = c(cartao_credito$AGE)
   ,DS_IDADE = cut(cartao_credito$AGE, breaks = c(0,12,18,60,100), 
-                 labels = c("crianca","adolescente","adulto","idoso"))
+                  labels = c("crianca","adolescente","adulto","idoso"))
   ,LIMITE = c(cartao_credito$LIMIT_BAL)
+  ,DS_LIMITE = cut(cartao_credito$LIMIT_BAL
+                   , breaks = c(0,49999,99999,499999,99999999)
+                   , labels = c("silver","gold","platium","black")
+                   
   )
+  ,PROPENS = c(cartao_credito$default.payment.next.month)
+)
 
-base_origem <- analise
-
-head(analise)
-tail(analise)
 
 #verifica dados ausentes na base de analise
 md.pattern(analise)
 
 #Retirando dados ausentes
-analise <- analise[complete.cases(analise), ]
+analise_sem_ausentes <- analise[complete.cases(analise), ]
 
-md.pattern(analise)
+md.pattern(analise_sem_ausentes)
 
-head(analise)
+dim(analise_sem_ausentes)
 
 #realizando normalizacao em limiite por z-score
-analise$LIMITE <- scale(analise$LIMITE)
+analise_sem_ausentes$LIMITE_NORM <- scale(analise_sem_ausentes$LIMITE)
 
-head(analise)
+head(analise_sem_ausentes)
 
 #indetificando outliers
-summary(analise)
 
-iqr_lim <- IQR(analise$LIMITE)
-iqr_lim
+summary(analise_sem_ausentes) #pegar o 1Q e 3Q para o calculo de limites
 
-li_lim <- -0.905483 - 1.5*iqr_lim
-ls_lim <- 0.558898 + 1.5*iqr_lim
+IQR(analise_sem_ausentes$LIMITE_NORM) 
 
-data.frame(which(analise$LIMITE < li_lim))
-data.frame(which(analise$LIMITE > ls_lim))
+li <- -0.904624 - 1.5*IQR(analise_sem_ausentes$LIMITE_NORM)
+li
+ls <- 0.557544 + 1.5*IQR(analise_sem_ausentes$LIMITE_NORM)
+ls
 
-data.frame(analise$LIMITE[which(analise$LIMITE < li_lim)])
+boxplot(analise_sem_ausentes$LIMITE_NORM) #boxplot considerando  os outliers
 
-#identificando outlier com cooks
-modelo <- lm(analise$LIMITE~., data = analise)
+#dados que estao abaixo do limite inferior
+data.frame(analise_sem_ausentes$LIMITE_NORM[which(analise_sem_ausentes$LIMITE_NORM < li)])
+#dados que estao acima  do limite superior
+data.frame(analise_sem_ausentes$LIMITE_NORM[which(analise_sem_ausentes$LIMITE_NORM > ls)])
+
+#filtrando os dados que estao acima do limite superior (outlier)
+outlier_ls <- filter(analise_sem_ausentes, LIMITE_NORM >ls )
+
+#retirandoo os outlier
+analise_sem_ausentes_e_outlier <- filter(analise_sem_ausentes, LIMITE_NORM <= ls )
+
+boxplot(analise_sem_ausentes_e_outlier$LIMITE_NORM)
+
+dim(analise_sem_ausentes_e_outlier)
+
+summary(analise_sem_ausentes_e_outlier)
+
+
+#identificando outlier com cooks (considerando multivariaveis)
+modelo <- lm(analise_sem_ausentes$LIMITE_NORM~., data = analise_sem_ausentes)
 distancia <- cooks.distance(modelo)
-plot(distancia, pch = "*", cex = 2, main = "influencia sengudo a distancia LIMITE")
+plot(distancia, pch = "*", cex = 2, main = "influencia sengudo a distancia")
 abline(h = 4 * mean(distancia, na.rm = T), col = "red")
 
 #separando os elementos influentes
@@ -70,26 +87,10 @@ influentes <- as.numeric(names(distancia)[(distancia > 4 * mean(distancia, na.rm
 influentes
 
 #verficando na base quem sao eles
-head(analise[influentes, ])
+head(analise_sem_ausentes[influentes, ])
 
-#retirando outlier da analise
-analise$LIMITE[analise$LIMITE > ls_lim] = ls_lim
-
-summary(analise$LIMITE)
+summary(analise_sem_ausentes)
 
 boxplot(analise$LIMITE)
 
 head(analise)
-
-
-
-summary(analise)
-
-modelo = lm(analise$LIMITE_NORM~.,data = analise)
-distancia = cooks.distance(modelo)
-plot(distancia, pch="*", cex=2, main="Influencia segundo a distancia")
-abline(h=4*mean(distancia,na.rm = T), col="red")
-
-influentes <- as.numeric(names(distancia)[(distancia > 4*mean(distancia, na.rm = T))])
-influentes
-
